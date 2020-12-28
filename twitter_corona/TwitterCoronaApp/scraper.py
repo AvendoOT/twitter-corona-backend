f3ad1bf53357b5e9a0c0b330ce43400a96c6699d
Leon Kranjcevic <lkranjcevic@croz.net>
2020-12-28 23:49:10 +0100

    Added twitter scrapper functionality and fixed design

---
+++ twitter_corona/TwitterCoronaApp/scraper.py
@@ -0,0 +1,13 @@
+import snscrape.modules.twitter as sntwitter
+
+
+def scraper(query, since, until):
+    tweets = []
+    for i, tweet in enumerate(
+            sntwitter.TwitterSearchScraper(query + ' since:' + since + ' until:' + until).get_items()):
+        if i > 100:
+            break
+        tweets.append({'date': tweet.date,
+                       'user': tweet.user.username,
+                       'tweet': tweet.content})
+    return tweets
--- twitter_corona/TwitterCoronaApp/views.py
+++ twitter_corona/TwitterCoronaApp/views.py
@@ -1,27 +1,18 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
-tweets = [
-    {
-        'date': '12.2.2020',
-        'user': 'dacxo',
-        'tweet': 'karona je prevara'
-    },
-    {
-        'date': '25.7.2020',
-        'user': 'fugo88',
-        'tweet': 'sezona gori'
-    },
-    {'date': '19.11.2020',
-     'user': 'antonio_markulinovic',
-     'tweet': 'crkla mi krava od karone'
-     }
-]
+from TwitterCoronaApp import scraper
 
 
 def home(request):
-    context = {'tweets': tweets}
-    return render(request, '../templates/TwitterCoronaApp/home.html', context)
+    if request.POST:
+        form_data = request.POST.dict()
+        twitter_scraper = scraper.scraper(form_data.get('tweet'), form_data.get('since'),
+                                          form_data.get('until'))
+        context = {'tweets': twitter_scraper}
+
+        return render(request, '../templates/TwitterCoronaApp/home.html', context)
+    else:
+        return render(request, '../templates/TwitterCoronaApp/home.html')
 
 
 def about(request):
