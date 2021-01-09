f501cebcb225da9a2cba5d67a11cb7aa15d47f89
lkranjcevic@croz.net <lkranjcevic@croz.net>
2021-01-10 00:48:04 +0100

    Added sentiment

---
+++ twitter_corona/TwitterCoronaApp/sentiment.py
@@ -0,0 +1,38 @@
+from textblob import TextBlob
+import sys
+import matplotlib.pyplot as plt
+
+
+def percentage(part, whole):
+    return 100 * float(part) / float(whole)
+
+
+def getSentiment(tweets, about):
+    positive = 0
+    neutral = 0
+    negative = 0
+    polarity = 0
+    length = len(tweets)
+    for tweet in tweets:
+        analysis = TextBlob(tweet)
+        polarity += analysis.sentiment.polarity
+        if analysis.sentiment.polarity == 0:
+            neutral += 1
+        elif analysis.sentiment.polarity > 0.00:
+            positive += 1
+        elif analysis.sentiment.polarity < 0.00:
+            negative += 1
+    positive = format(percentage(positive, length), '.2f')
+    negative = format(percentage(negative, length), '.2f')
+    neutral = format(percentage(neutral, length), '2f')
+
+    labels = ['Positive [' + str(positive) + '%]', 'Neutral [' + str(neutral) + '%]',
+              'Negative [' + str(negative) + '%]']
+    sizes = [positive, neutral, negative]
+    colors = ['yellowgreen', 'gold', 'red']
+    patches, texts = plt.pie(sizes, colors=colors, startangle=90)
+    plt.legend(patches, labels, loc="best")
+    plt.title("Reakcije ljudi na " + about)
+    plt.axis = 'equal'
+    plt.tight_layout()
+    plt.show()
--- twitter_corona/TwitterCoronaApp/views.py
+++ twitter_corona/TwitterCoronaApp/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 
 from TwitterCoronaApp import scraper
 
+from TwitterCoronaApp import sentiment
+
 
 def home(request):
     if request.POST:
@@ -9,6 +11,11 @@ def home(request):
         twitter_scraper = scraper.scraper(form_data.get('tweet'), form_data.get('since'),
                                           form_data.get('until'))
         context = {'tweets': twitter_scraper}
+        tweets_sentiment = []
+        for tw in twitter_scraper:
+            tweets_sentiment.append(tw.get('tweet'))
+
+        sentiment.getSentiment(tweets_sentiment, form_data.get('tweet'))
 
         return render(request, '../templates/TwitterCoronaApp/home.html', context)
     else:
