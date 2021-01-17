8d8d55a7026157ca3fd678b999ea48b6f7c51105
lkranjcevic@croz.net <lkranjcevic@croz.net>
2021-01-17 18:29:29 +0100

    Deploy #3

--- app.py
+++ app.py
@@ -1,12 +1,30 @@
 import os
 from flask import Flask
+from django.shortcuts import render
+
+from TwitterCoronaApp import scraper
+
+from TwitterCoronaApp import sentiment
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def hello():
-    return "Twitter Corona application"
+def hello(request):
+    if request.POST:
+        form_data = request.POST.dict()
+        twitter_scraper = scraper.scraper(form_data.get('tweet'), form_data.get('since'),
+                                          form_data.get('until'))
+        tweets_sentiment = []
+        for tw in twitter_scraper:
+            tweets_sentiment.append(tw.get('tweet'))
+
+        sentiment_image = sentiment.getSentiment(tweets_sentiment, form_data.get('tweet'))
+
+        return render(request, '../templates/TwitterCoronaApp/home.html', {'tweets': twitter_scraper,
+                                                                           'sentiment_image': sentiment_image})
+    else:
+        return render(request, '../templates/TwitterCoronaApp/home.html')
 
 
 if __name__ == "__main__":
