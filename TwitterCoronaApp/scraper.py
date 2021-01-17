bdeb13f22c1a0cd0ed7eb6046aff4e980a21f0a8
lkranjcevic@croz.net <lkranjcevic@croz.net>
2021-01-17 19:33:43 +0100

    Deploy fix

--- TwitterCoronaApp/scraper.py
+++ TwitterCoronaApp/scraper.py
@@ -3,10 +3,13 @@ import snscrape.modules.twitter as sntwitter
 
 def scraper(query, since, until):
     tweets = []
+    # since = datetime.strptime(since, '%Y-%m-%d')
+    # until = datetime.strptime(until, '%Y-%m-%d')
     for i, tweet in enumerate(
             sntwitter.TwitterSearchScraper(query + ' since:' + since + ' until:' + until).get_items()):
         if i > 100:
             break
+        # sntwitter.TwitterSearchScraper(query + ' since:' + since + ' until:' + until).get_items()):
         tweets.append({'date': tweet.date,
                        'user': tweet.user.username,
                        'tweet': tweet.content})
